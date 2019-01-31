2c1d922e45e9e258866fc2ab38501ec18ab4849e
s63-marc <marc@stack63.com>
2019-01-31 18:45:54 +0800

    Generate build;

--- src/index.js
+++ src/index.js
@@ -1,232 +1,249 @@
 // RedDotPay Hosted Page JavaScript Development Kit
 
 const config = {
-	authDomain: "https://rg4fgp2oag.execute-api.ap-southeast-1.amazonaws.com/prod/v1",
-	domain: "http://localhost:8080",
-	popup: {
-		title: "Red Dot Pay",
-		width: 800,
-		height: 600,
-	},
-	statusCheckFrequency: 3*1000 // every 3 seconds
-}
+  authDomain: "https://connect.api.reddotpay.sg/v1",
+  domain: "https://connect.reddotpay.sg",
+  popup: {
+    title: "Red Dot Pay",
+    width: 800,
+    height: 600
+  },
+  statusCheckFrequency: 3 * 1000 // every 3 seconds
+};
 
 const popup = null;
 
 export default class Pay {
-	constructor({
-		merchantId,
-		clientId,
-		secret,
-		payload
-	}) {
-		console.log(merchantId, clientId, secret, payload)
-		this.merchantId = merchantId;
-		this.clientId = clientId;
-		this.secret = secret;
-		this.payload = payload;
-		this.initialized = false;
-		this.accessToken = null;
+  constructor({ merchantId, clientId, secret, payload }) {
+    console.log(merchantId, clientId, secret, payload);
+    this.merchantId = merchantId;
+    this.clientId = clientId;
+    this.secret = secret;
+    this.payload = payload;
+    this.initialized = false;
+    this.accessToken = null;
 
-		this.popup = popup;
+    this.popup = popup;
 
-		console.log("merchantId >>>", merchantId)
-	}
+    console.log("merchantId >>>", merchantId);
+  }
 
-	init() {
-		if (this.initialized) {
-			this.destroy();
-		}
+  init() {
+    if (this.initialized) {
+      this.destroy();
+    }
 
-		this.pay();
+    this.pay();
 
-		this.initialized = true;
+    this.initialized = true;
+  }
 
-	}
+  async _verifyMerchant(merchantId) {
+    console.log("verifyMerchant >>>>>>>>>>>>>", merchantId);
 
-	async _verifyMerchant(merchantId) {
-		console.log("verifyMerchant >>>>>>>>>>>>>", merchantId)
+    let data = await fetch(`${config.authDomain}/merchants/${merchantId}`, {
+      method: "GET",
+      credentials: "same-origin",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      }
+    });
 
-		let data = await fetch(
-			`${config.authDomain}/merchants/${merchantId}`, {
-				method: "GET",
-				credentials: "same-origin",
-				mode: "cors",
-				headers: {
-					"Content-Type": "application/json; charset=utf-8"
-				}
-			}
-		)
+    return await data.json();
+  }
 
-		return await data.json();
-	}
+  async _getAccessToken(client, secret) {
+    console.log("getAccessToken >>>>>>>>>>>>>", client, secret);
+    let data = await fetch(`${config.authDomain}/authenticate`, {
+      method: "POST",
+      accept: "application/json",
+      credentials: "same-origin",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+        // "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: JSON.stringify({
+        clientId: client,
+        secret: secret
+      })
+    });
 
-	async _getAccessToken(client, secret) {
-		console.log("getAccessToken >>>>>>>>>>>>>", client, secret)
-		let data = await fetch(
-			`${config.authDomain}/authenticate`, {
-				method: "POST",
-				accept: "application/json",
-				credentials: "same-origin",
-				mode: "cors",
-				headers: {
-					"Content-Type": "application/json; charset=utf-8"
-					// "Content-Type": "application/x-www-form-urlencoded",
-				},
-				body: JSON.stringify({
-					clientId: client,
-					secret: secret
-				})
-			}
-		)
+    return await data.json();
+  }
 
-		return await data.json();
-	}
+  async _getPaymentStatus(mid, payment_ref) {
+    console.log("getPaymentStatus >>>>>>>>>>>>>", mid, payment_ref);
+    let data = await fetch(
+      `${config.authDomain}/payments/token/${mid}/status/${payment_ref}`
+    );
 
-	async _getPaymentStatus(mid, payment_ref) {
-		console.log("getPaymentStatus >>>>>>>>>>>>>", mid, payment_ref)
-		let data = await fetch(
-				`${config.authDomain}/payments/token/${mid}/status/${payment_ref}`
-			)
+    return await data.json();
+  }
 
-		return await data.json();
-	}
+  async _getPaymentToken(accessToken, merchantId, payload) {
+    console.log(
+      "_getPaymentToken >>>>>>>>>>>>>",
+      accessToken,
+      merchantId,
+      payload
+    );
+    let data = await fetch(
+      `${config.authDomain}/payments/token/${merchantId}`,
+      {
+        method: "POST",
+        credentials: "same-origin",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          Authorization: `Bearer ${accessToken}`
+          // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: JSON.stringify(payload)
+      }
+    );
 
-	async _getPaymentToken(accessToken, merchantId, payload) {
-		console.log("_getPaymentToken >>>>>>>>>>>>>", accessToken, merchantId, payload)
-		let data = await fetch(
-			`${config.authDomain}/payments/token/${merchantId}`, {
-				method: "POST",
-				credentials: "same-origin",
-				mode: "cors",
-				headers: {
-					"Content-Type": "application/json; charset=utf-8",
-					"Authorization": `Bearer ${accessToken}`
-					// "Content-Type": "application/x-www-form-urlencoded",
-				},
-				body: JSON.stringify(payload)
-			}
-		)
+    return await data.json();
+  }
 
-		return await data.json();
-	}
+  openPopup() {
+    // PopupCenter
 
-	openPopup() {
-		// PopupCenter
+    // let url =  `http://view-pay-redirect.herokuapp.com/pay/verify`;//,"MyWindow", 1024, 768)//"config='toolbar=no, menubar=no,scrollbars=no,resizable=no,location=no,directories=no,status=no'")
+    let url = `${config.domain}/m/${this.merchantId}/#/pay`;
 
-		// let url =  `http://view-pay-redirect.herokuapp.com/pay/verify`;//,"MyWindow", 1024, 768)//"config='toolbar=no, menubar=no,scrollbars=no,resizable=no,location=no,directories=no,status=no'")
-		let url = `${config.domain}/m/${this.merchantId}/#/pay`;
+    // START: This block makes sure that the popup window is in the center of the page
+    let dualScreenLeft =
+      window.screenLeft != undefined ? window.screenLeft : screen.left;
+    let dualScreenTop =
+      window.screenTop != undefined ? window.screenTop : screen.top;
 
-		// START: This block makes sure that the popup window is in the center of the page
-		let dualScreenLeft =
-			window.screenLeft != undefined ? window.screenLeft : screen.left;
-		let dualScreenTop =
-			window.screenTop != undefined ? window.screenTop : screen.top;
+    let width = window.innerWidth
+      ? window.innerWidth
+      : document.documentElement.clientWidth
+      ? document.documentElement.clientWidth
+      : screen.width;
+    let height = window.innerHeight
+      ? window.innerHeight
+      : document.documentElement.clientHeight
+      ? document.documentElement.clientHeight
+      : screen.height;
 
-		let width = window.innerWidth ?
-			window.innerWidth :
-			document.documentElement.clientWidth ?
-			document.documentElement.clientWidth :
-			screen.width;
-		let height = window.innerHeight ?
-			window.innerHeight :
-			document.documentElement.clientHeight ?
-			document.documentElement.clientHeight :
-			screen.height;
+    let left = width / 2 - config.popup.width / 2 + dualScreenLeft;
+    let top = height / 2 - config.popup.height / 2 + dualScreenTop + 15;
+    this.popup = window.open(
+      url,
+      config.popup.title,
+      "scrollbars=yes, width=" +
+        config.popup.width +
+        ", height=" +
+        config.popup.height +
+        ", top=" +
+        top +
+        ", left=" +
+        left
+    );
 
-		let left = width / 2 - config.popup.width / 2 + dualScreenLeft;
-		let top = height / 2 - config.popup.height / 2 + dualScreenTop + 15;
-		this.popup = window.open(
-			url,
-			config.popup.title,
-			"scrollbars=yes, width=" +
-			config.popup.width +
-			", height=" +
-			config.popup.height +
-			", top=" +
-			top +
-			", left=" +
-			left
-		);
+    // END: This block makes sure that the popup window is in the center of the page
 
-		// END: This block makes sure that the popup window is in the center of the page
+    // Puts focus on the newWindow
+    if (window.focus) {
+      this.popup.focus();
+    }
+  }
 
-		// Puts focus on the newWindow
-		if (window.focus) {
-			this.popup.focus();
-		}
-	}
+  async pay() {
+    console.log(">>>>>>>>>> pay");
+    this.openPopup();
+    // let verifyMerchant = await this._verifyMerchant()
 
-	async pay() {
-		console.log(">>>>>>>>>> pay")
-		this.openPopup();
-		// let verifyMerchant = await this._verifyMerchant()
+    let verifyMerchant = await this._verifyMerchant(this.merchantId).catch(
+      err => {
+        throw new Error(
+          "Something went wrong while validating the merchant: ",
+          err
+        );
+        this.popup.location.href = `${config.domain}/m/${
+          this.merchantId
+        }/#/500`;
+      }
+    );
+    if (typeof verifyMerchant === "object") {
+      let accessToken = await this._getAccessToken(this.clientId, this.secret);
+      console.log(accessToken);
 
-		let verifyMerchant = await this._verifyMerchant(this.merchantId).catch(err => {
-			throw new Error("Something went wrong while validating the merchant: ", err)
-			this.popup.location.href = `${config.domain}/m/${this.merchantId}/#/500`;
+      this._getPaymentToken(
+        accessToken.accessToken,
+        this.merchantId,
+        this.payload
+      )
+        .then(response => {
+          // redirect to new url
+          if (response.token === undefined) {
+            this.popup.location.href = `${config.domain}/m/${
+              this.merchantId
+            }/#/500`;
+            throw new Error("Something went wrong: Token is not defined");
+          } else {
+            this.popup.location.href = `${config.domain}/m/${
+              this.merchantId
+            }/#/pay/${response.token}`;
 
-		})
-		if (typeof verifyMerchant === "object") {
-			let accessToken = await this._getAccessToken(this.clientId, this.secret);
-			console.log(accessToken);
+            // Remote development environment or your production env
+            // this.popup.location.href = `http://connect.reddotpay.sg/#/pay/${merchantId}/${data.token}`;
 
-			this._getPaymentToken(accessToken.accessToken, this.merchantId, this.payload)
-				.then(response => {
-					// redirect to new url
-					if (response.token === undefined) {
-						this.popup.location.href = `${config.domain}/m/${this.merchantId}/#/500`;
-						throw new Error("Something went wrong: Token is not defined")
-					} else {
-						this.popup.location.href = `${config.domain}/m/${this.merchantId}/#/pay/${response.token}`;
+            // Start timer to check payment status if = `captured`
 
-						// Remote development environment or your production env
-						// this.popup.location.href = `http://connect.reddotpay.sg/#/pay/${merchantId}/${data.token}`;
+            let statusInterval = setInterval(() => {
+              this._getPaymentStatus(
+                this.merchantId,
+                this.payload.orderId
+              ).then(response => {
+                if (response.status == "capture") {
+                  clearInterval(statusInterval);
+                  console.log("Status: ", response.status);
+                  // statusBox.innerText = "Order paid!";
+                  // $("#success").show();
+                  // $("#loading").hide();
+                  document.location.href = "/";
+                } else if (
+                  response.status == "pending" ||
+                  response.status == "authorize"
+                ) {
+                  console.log("Status: ", response.status);
+                  // statusBox.innerText = "Waiting for response from the checkout page...";
+                } else {
+                  clearInterval(interval);
+                  console.log("Status: ", response.status);
+                  // statusBox.innerText = "Something went wrong... Please try again.";
+                }
+              });
+            }, config.statusCheckFrequency);
+          }
+        })
+        .catch(err => {
+          this.popup.location.href = `${config.domain}/m/${
+            this.merchantId
+          }/#/500`;
+          throw new Error(
+            "Something went wrong while getting the payment token: ",
+            err
+          );
+        });
+    }
 
-						// Start timer to check payment status if = `captured`
+    // this._getAccessToken(this.clientId, this.secret)
+    // 	.then(response => {
+    // 		this.accessToken = response.accessToken
+    // 		this._getPaymentToken()
+    // 	})
+  }
 
-						let statusInterval = setInterval(() => {
-							this._getPaymentStatus(this.merchantId, this.payload.orderId)
-								.then((response)=> {
-									if (response.status == "capture") {
-										clearInterval(statusInterval);
-										console.log("Status: ", response.status);
-										// statusBox.innerText = "Order paid!";
-										// $("#success").show();
-										// $("#loading").hide();
-										document.location.href = "/";
-									} else if (
-										response.status == "pending" ||
-										response.status == "authorize"
-									) {
-										console.log("Status: ", response.status);
-										// statusBox.innerText = "Waiting for response from the checkout page...";
-									} else {
-										clearInterval(interval);
-										console.log("Status: ", response.status);
-										// statusBox.innerText = "Something went wrong... Please try again.";
-									}
-								})
-								}, config.statusCheckFrequency);
-					}
-				})
-				.catch(err => {
-					this.popup.location.href = `${config.domain}/m/${this.merchantId}/#/500`;
-					throw new Error('Something went wrong while getting the payment token: ', err)
-				})
-		}
+  destroy() {
+    // assign to `null` elements that are creted by this initially
 
-		// this._getAccessToken(this.clientId, this.secret)
-		// 	.then(response => {
-		// 		this.accessToken = response.accessToken
-		// 		this._getPaymentToken()
-		// 	})
-	}
-
-	destroy() {
-		// assign to `null` elements that are creted by this initially
-
-		// unset initialized value to true
-		this.initialized = false;
-	}
+    // unset initialized value to true
+    this.initialized = false;
+  }
 }
